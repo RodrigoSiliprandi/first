4058ce9c06279754a9a6e06fd0519fbb5a391941
RodrigoSiliprandi <rodrigosiliprandi.si@gmail.com>
2019-04-21 18:43:03 -0300

    Curso, Disciplina, Aula

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :aulas
+  resources :disciplinas
+  resources :cursos
   resources :professores
   resources :professors
   resources :alunos
---
+++ db/migrate/20190421213333_create_aulas.rb
@@ -0,0 +1,12 @@
+class CreateAulas < ActiveRecord::Migration[5.2]
+  def change
+    create_table :aulas do |t|
+      t.disciplina :disciplina
+      t.date :data
+      t.string :objetivo
+      t.string :realizado
+
+      t.timestamps
+    end
+  end
+end
